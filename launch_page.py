cafcdeb316a7dbe7125d1565fb1a6819f2de386c
Yosi1602 <agatayosephine@gmail.com>
2023-12-12 15:50:53 +0100

    Adjusting appearance

--- launch_page.py
+++ launch_page.py
@@ -3,6 +3,6 @@ import landing_page
 
 
 with gr.Blocks(theme=landing_page.Custom()) as demo:
-    landing_page.renderDesign()
+    landing_page.renderDesign() # Rendering the landing page
   
 demo.launch()
